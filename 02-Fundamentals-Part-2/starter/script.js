6f5081f9875e65299e76b412068fcfa1de498b5d
Lukenge Faridu Musa <95524786+Musa1990f@users.noreply.github.com>
2022-06-18 03:07:01 +0900

    Added array methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -40,34 +40,34 @@
 //   console.log(age1,age2);
 
 //Arrow function 
-const calcAge3 = birthYear => 2044 - birthYear; 
-const age3 = calcAge3(1990);
-console.log(age3);
+// const calcAge3 = birthYear => 2044 - birthYear; 
+// const age3 = calcAge3(1990);
+// console.log(age3);
 
-// Using arrow functions with more parameters
+// // Using arrow functions with more parameters
 
-const retirementAge = (birthYear, firstName) => {
-  const age4 = 2037 - birthYear;
-   const retirement = 65 -age4;
-  // // return retirementAge;
-   return `${firstName} retires in ${birthYear}`
-}
- console.log(retirementAge(1991, 'Musa'));
+// const retirementAge = (birthYear, firstName) => {
+//   const age4 = 2037 - birthYear;
+//    const retirement = 65 -age4;
+//   // // return retirementAge;
+//    return `${firstName} retires in ${birthYear}`
+// }
+//  console.log(retirementAge(1991, 'Musa'));
 
- // Functions calling other functions
+//  // Functions calling other functions
 
-function cutFruitPieces(fruit){
-  return fruit *  4;
-}
+// function cutFruitPieces(fruit){
+//   return fruit *  4;
+// }
 
- function fruitProccessor (appples, oranges){
-  const applePieces = cutFruitPieces(appples);
-  const orangePieces = cutFruitPieces(oranges)
-    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
-     return juice;
-  }
+//  function fruitProccessor (appples, oranges){
+//   const applePieces = cutFruitPieces(appples);
+//   const orangePieces = cutFruitPieces(oranges)
+//     const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+//      return juice;
+//   }
 
-  console.log(fruitProccessor(4,6));
+//   console.log(fruitProccessor(4,6));
 
  // Examples of  functions
 
@@ -99,49 +99,78 @@ function cutFruitPieces(fruit){
 
 // **********ARRAYS***********
 
+// // const friends = ['Micheal', 'John', 'James'];
+// console.log(friends);
+
+// // This can also be used!!
+// const yr = new Array(1999, 2000, 2374);
+// console.log(yr);
+
+// // How to get out Arrays!
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// // How to find actul number in a Array!
+// console.log(friends.length);
+
+// // how to get index fro the last array!!
+// console.log(friends[friends.length - 1]);
+
+// // How to change or mutate an array!!
+
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// // Array can hold different types of values at the same time!!
+// const firstName = 'musa';
+// const musa = [firstName,'faridu', 2022 - 1990, friends];
+// console.log(musa.length);
+
+// // Exercise
+
+// const calcAge = function(birthYear) {
+//      return 2044 - birthYear;
+//      }
+
+// const years = [1999,1987,1890,];
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age4 = calcAge(years[years.length - 1 ]);
+// console.log(age1,age2,age4);
+
+// const ages = [calcAge(years[0]),calcAge(years[1]),calcAge(years[years.length - 1])];
+// console.log(ages);
+
+// Array methods 
 const friends = ['Micheal', 'John', 'James'];
+
+// adds elements into array!!
+friends.push('musa');
+console.log(friends);
+const newLength = friends.push('musa');
+
+friends.unshift('John');
 console.log(friends);
 
-// This can also be used!!
-const yr = new Array(1999, 2000, 2374);
-console.log(yr);
+// removes elements
+friends.pop();
+console.log(friends);
+const popped = friends.pop();
+console.log(popped);
 
-// How to get out Arrays!
-console.log(friends[0]);
-console.log(friends[2]);
-
-// How to find actul number in a Array!
-console.log(friends.length);
-
-// how to get index fro the last array!!
-console.log(friends[friends.length - 1]);
-
-// How to change or mutate an array!!
-
-friends[2] = 'Jay';
+friends.shift();
 console.log(friends);
 
-// Array can hold different types of values at the same time!!
-const firstName = 'musa';
-const musa = [firstName,'faridu', 2022 - 1990, friends];
-console.log(musa.length);
+console.log(friends.indexOf('Micheal'));
+console.log(friends.indexOf('Bob'));
 
-// Exercise
+// Includes is used for conditions
+console.log(friends.includes('John'));
+console.log(friends.includes('Bob'));
 
-const calcAge = function(birthYear) {
-     return 2044 - birthYear;
-     }
-
-const years = [1999,1987,1890,];
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age4 = calcAge(years[years.length - 1 ]);
-console.log(age1,age2,age4);
-
-const ages = [calcAge(years[0]),calcAge(years[1]),calcAge(years[years.length - 1])];
-console.log(ages);
-
-
+if (friends.includes('James')) {
+  console.log('You have a friend called James');
+}
 
 
 
